354aafbaa72919529f16ec2ade8d8ddb47e7dacc
Younes SADMI <younes.sadmi@gmail.com>
2016-05-02 22:16:06 +0200

    first commit

---
+++ views/layout/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $this->getTitle();?></title>        
+        <link rel="icon" href="<?php echo BASE_URL;?>img/favicon.ico">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+        <link href="<?php echo BASE_URL;?>css/mystyle.css" rel="stylesheet" type="text/css">
+        <script src="<?php echo BASE_URL;?>js/functions.js"></script>
+    </head>
+    <body>
+        <header>
+            
+        </header>
