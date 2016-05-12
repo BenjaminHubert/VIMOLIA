ef6891a6cf9c0eaf2e6db73fae71bbae27bd90ec
Younes SADMI <younes.sadmi@gmail.com>
2016-05-12 22:18:29 +0200

    materialize template

--- views/layout/header.php
+++ views/layout/header.php
@@ -6,11 +6,41 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <!-- JQUERY -->
+        <script src="<?php echo BASE_URL;?>js/jquery-2.1.1.min.js"></script>
+
+        <!-- MATERIALIZE -->
+        <link href="<?php echo BASE_URL;?>css/material-icon.css" rel="stylesheet">
+        <link href="<?php echo BASE_URL;?>css/materialize.min.css" rel="stylesheet" type="text/css">
+        <script src="<?php echo BASE_URL;?>js/materialize.min.js"></script>
 
         <link href="<?php echo BASE_URL;?>css/mystyle.css" rel="stylesheet" type="text/css">
         <script src="<?php echo BASE_URL;?>js/functions.js"></script>
     </head>
     <body>
-        <header>
-            
-        </header>
+        <nav class="light-blue lighten-1" role="navigation">
+            <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#">Navbar Link</a></li>
+                </ul>
+
+                <ul id="nav-mobile" class="side-nav">
+                    <li><a href="#">Navbar Link</a></li>
+                </ul>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+        <div class="section no-pad-bot" id="index-banner">
+            <div class="container">
+                <br><br>
+                <h1 class="header center orange-text">Starter Template</h1>
+                <div class="row center">
+                    <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                </div>
+                <div class="row center">
+                    <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+                </div>
+                <br><br>
+
+            </div>
+        </div>
