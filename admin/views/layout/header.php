27572aa23c78e2c93091c2f918eaaeb9bba1d153
Younes SADMI <younes.sadmi@gmail.com>
2016-05-14 13:05:05 +0200

    cleaning admin directory

--- admin/views/layout/header.php
+++ admin/views/layout/header.php
@@ -14,34 +14,8 @@
         <link href="<?php echo BASE_URL;?>css/materialize.min.css" rel="stylesheet" type="text/css">
         <script src="<?php echo BASE_URL;?>js/materialize.min.js"></script>
 
-        <link href="<?php echo BASE_URL;?>css/mystyle.css" rel="stylesheet" type="text/css">
-        <script src="<?php echo BASE_URL;?>js/functions.js"></script>
+        <link href="<?php echo BASE_URL_ADMIN;?>css/mystyle.css" rel="stylesheet" type="text/css">
+        <script src="<?php echo BASE_URL_ADMIN;?>js/functions.js"></script>
     </head>
     <body>
-        <nav class="light-blue lighten-1" role="navigation">
-            <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="#">Navbar Link</a></li>
-                </ul>
-
-                <ul id="nav-mobile" class="side-nav">
-                    <li><a href="#">Navbar Link</a></li>
-                </ul>
-                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-            </div>
-        </nav>
-        <div class="section no-pad-bot" id="index-banner">
-            <div class="container">
-                <br><br>
-                <h1 class="header center orange-text">Starter Template</h1>
-                <div class="row center">
-                    <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-                </div>
-                <div class="row center">
-                    <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
-                </div>
-                <br><br>
-
-            </div>
-        </div>
         <div class="container">
