e94b2996da2c56419fc74652dfe9738f4c003742
Younes SADMI <younes.sadmi@gmail.com>
2016-05-22 18:03:13 +0200

    utilisateurs rubric added

--- admin/views/layout/header.php
+++ admin/views/layout/header.php
@@ -29,6 +29,7 @@
                         <li><a href="<?php echo BASE_URL_ADMIN;?>monCompte">Mon compte</a></li>
                         <li><a href="<?php echo BASE_URL_ADMIN;?>article">Articles</a></li>
                         <li><a href="<?php echo BASE_URL_ADMIN;?>page">Pages</a></li>
+                        <li><a href="<?php echo BASE_URL_ADMIN;?>utilisateur">Utilisateurs</a></li>
                     </ul>
                     <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
                 </div>
