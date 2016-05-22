61bc21dcdd76949c3f189269fe3ec22d9d3c75d4
Younes SADMI <younes.sadmi@gmail.com>
2016-05-22 17:37:36 +0200

    administration part only available if logged

--- admin/application/baseController.class.php
+++ admin/application/baseController.class.php
@@ -3,8 +3,12 @@ Abstract Class baseController {
 
     protected $registry;
 
-    function __construct($registry) {
+    function __construct($registry){
         $this->registry = $registry;
+        if(!isset($_SESSION['id'])){
+            header('Location: '.BASE_URL.'login');
+            die();
+        }
     }
 
     abstract function index();
