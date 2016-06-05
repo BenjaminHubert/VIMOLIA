84e31ffaf9fb7b5194bfaad023bb9885a1275d15
Thibault Lenormand <superskoller@gmail.com>
2016-06-05 19:03:35 +0200

    un bon début

--- admin/controller/article.php
+++ admin/controller/article.php
@@ -1,14 +1,37 @@
 <?php
 class articleController extends baseController {
 
-    public function index(){        
+    public function index(){
+        // Liste des articles
+        $this->registry->template->listArticle = $this->registry->db->getListArticle();
         $this->registry->template->show('index');
     }
-    public function add(){        
+    
+    public function add($args){   
+        // Ajouter un article
+        if(isset($_POST['submit']) && $_POST['submit'] == 'create'){
+            
+            $this->registry->db->addArticle($article);
+            $this->registry->template->show('index');
+        }
         $this->registry->template->show('add');
     }
-    public function edit(){        
-        $this->registry->template->show('edit');
+    
+    public function edit($args){
+        // Modifier un article
+        if(isset($_POST['submit']) && $_POST['submit'] == 'update'){
+            
+            $this->registry->db->editArticle($article);
+            $this->registry->template->show('index');
+        }
+
+        // Affichage par défaut
+        if(isset($args[0]) && is_numeric($args[0])){
+            if(!empty($article = $this->registry->db->getArticleById($args[0]))){
+                $this->registry->template->article = $article;
+                $this->registry->template->show('article/edit');
+            }else $this->registry->template->show('not_found');
+        }else $this->registry->template->show('not_found');
     }
 }
 ?>
--- admin/controller/page.php
+++ admin/controller/page.php
@@ -1,14 +1,37 @@
 <?php
 class pageController extends baseController {
 
-    public function index(){        
+    public function index(){
+        // Liste des pages
+        $this->registry->template->listPage = $this->registry->db->getListPage();
         $this->registry->template->show('index');
     }
-    public function add(){        
+    
+    public function add($args){   
+        // Ajouter une page
+        if(isset($_POST['submit']) && $_POST['submit'] == 'create'){
+            
+            $this->registry->db->addPage($page);
+            $this->registry->template->show('index');
+        }
         $this->registry->template->show('add');
     }
-    public function edit(){        
-        $this->registry->template->show('edit');
+    
+    public function edit($args){
+        // Modifier une page
+        if(isset($_POST['submit']) && $_POST['submit'] == 'update'){
+            
+            $this->registry->db->editPage($page);
+            $this->registry->template->show('index');
+        }
+
+        // Affichage par défaut
+        if(isset($args[0]) && is_numeric($args[0])){
+            if(!empty($page = $this->registry->db->getPageById($args[0]))){
+                $this->registry->template->page = $page;
+                $this->registry->template->show('page/edit');
+            }else $this->registry->template->show('not_found');
+        }else $this->registry->template->show('not_found');
     }
 }
 ?>
