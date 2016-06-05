f2294bc11ed8e72622ca8c9edc46a1b624cc8ad3
Younes SADMI <younes.sadmi@gmail.com>
2016-06-05 13:20:17 +0200

    avatar added on the expert answer

--- views/question/afficher.php
+++ views/question/afficher.php
@@ -13,10 +13,17 @@
 <div class="row">
 <?php if($answers){?>
 <?php foreach($answers as $answer){?>
-	<div class="col s12 card grey">
-		<p><?php echo $answer['answer_text'];?></p>
-		<p style="text-align:right"><?php echo ($answer['pseudo'] !== NULL)?$answer['pseudo']:$answer['first_name'].' '.$answer['last_name'];?>, expert <?php echo APP_TITLE;?></p>
+<div class="col s12 card grey">
+	<div class="row">
+		<div class="col s2">
+			<img style="width:100%; margin:10px" src="<?php echo ($answer['url_avatar'] !== NULL)?$answer['url_avatar']:BASE_URL.'img/avatar/user.png';?>" alt="">
+		</div>
+		<div class="col s10">
+			<p><?php echo $answer['answer_text'];?></p>
+			<p style="text-align:right"><?php echo ($answer['pseudo'] !== NULL)?$answer['pseudo']:$answer['first_name'].' '.$answer['last_name'];?>, expert <?php echo APP_TITLE;?></p>
+		</div>
 	</div>
+</div>
 <?php }?>
 <?php }else{?>
 	<div class="col s12">
