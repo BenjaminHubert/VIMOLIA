314129e307e11cb14c48b6d2559fd273be840d05
Younes SADMI <younes.sadmi@gmail.com>
2016-06-13 17:22:57 +0200

    back and front are now ISO

--- admin/js/functions.js
+++ admin/js/functions.js
@@ -1,22 +1,28 @@
-// datepicker french transalation
+//datepicker french transalation
 jQuery.extend( jQuery.fn.pickadate.defaults, {
-    monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
-    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
-    weekdaysFull: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
-    weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
-    today: 'Aujourd\'hui',
-    clear: 'Effacer',
-    close: 'Fermer',
-    firstDay: 1,
-    format: 'dd mmmm yyyy',
-    formatSubmit: 'yyyy-mm-dd',
-    labelMonthNext:"Mois suivant",
-    labelMonthPrev:"Mois précédent",
-    labelMonthSelect:"Sélectionner un mois",
-    labelYearSelect:"Sélectionner une année"
+	monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
+	monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
+	weekdaysFull: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
+	weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
+	today: 'Aujourd\'hui',
+	clear: 'Effacer',
+	close: 'Fermer',
+	firstDay: 1,
+	format: 'dd mmmm yyyy',
+	formatSubmit: 'yyyy-mm-dd',
+	labelMonthNext:"Mois suivant",
+	labelMonthPrev:"Mois précédent",
+	labelMonthSelect:"Sélectionner un mois",
+	labelYearSelect:"Sélectionner une année"
 });
 
 $(document).ready(function(){
-    $('.button-collapse').sideNav();
-    $('textarea[lenght], input[lenght]').characterCounter();
+	$('.button-collapse').sideNav({
+		menuWidth: 300, // Default is 240
+		edge: 'left', // Choose the horizontal origin
+		closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	}
+	);
+	$(".dropdown-button").dropdown();
+	$('textarea[lenght], input[lenght]').characterCounter();
 });
--- views/layout/header.php
+++ views/layout/header.php
@@ -1,72 +1,23 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title><?php echo $this->getTitle();?></title>        
-        <link rel="icon" href="<?php echo BASE_URL;?>img/favicon.ico">
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+<title><?php echo $this->getTitle();?></title>
+<link rel="icon" href="<?php echo BASE_URL;?>img/favicon.ico">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- JQUERY -->
-        <script src="<?php echo BASE_URL;?>js/jquery-2.1.1.min.js"></script>
+<!-- JQUERY -->
+<script src="<?php echo BASE_URL;?>js/jquery-2.1.1.min.js"></script>
 
-        <!-- MATERIALIZE -->
-        <link href="<?php echo BASE_URL;?>css/material-icon.css" rel="stylesheet">
-        <link href="<?php echo BASE_URL;?>css/materialize.min.css" rel="stylesheet" type="text/css">
-        <script src="<?php echo BASE_URL;?>js/materialize.min.js"></script>
+<!-- MATERIALIZE -->
+<link href="<?php echo BASE_URL;?>css/material-icon.css" rel="stylesheet">
+<link href="<?php echo BASE_URL;?>css/materialize.min.css" rel="stylesheet" type="text/css">
+<script src="<?php echo BASE_URL;?>js/materialize.min.js"></script>
 
-        <!-- CUSTOME CSS & JS -->
-        <link href="<?php echo BASE_URL;?>css/mystyle.css" rel="stylesheet" type="text/css">
-        <script src="<?php echo BASE_URL;?>js/functions.js"></script>
-    </head>
-    <body>
-        <?php if(isset($_SESSION['id'])){?>
-        <ul id="dropdown1" class="dropdown-content">
-            <li><a href="<?php echo BASE_URL_ADMIN;?>"><i class="material-icons left">account_circle</i> Mon compte</a></li>
-            <li><a href="<?php echo BASE_URL;?>search/praticien"><i class="material-icons left">search</i> Praticiens</a></li>
-            <li><a href="<?php echo BASE_URL;?>question"><i class="material-icons left">question_answer</i> Questions</a></li>
-            <li class="divider"></li>
-            <li><a href="<?php echo BASE_URL;?>login/logout"><i class="material-icons left">lock_open</i> Déconnexion</a></li>
-        </ul>
-        <?php }?>
-        <nav class="light-blue lighten-1" role="navigation">
-            <div class="nav-wrapper container">
-                <a id="logo-container" href="<?php echo BASE_URL;?>" class="brand-logo hide-on-med-and-down">
-                    <img src="<?php echo BASE_URL;?>img/logo.png" alt="<?php echo APP_TITLE;?>" >
-                </a>
-                <ul class="right">
-                    <?php if(!isset($_SESSION['id'])){?>
-					<div class="hide-on-med-and-down">
-	                    <li><a href="<?php echo BASE_URL;?>search/praticien"><i class="material-icons left">search</i> Praticiens</a></li>
-	                    <li><a href="<?php echo BASE_URL;?>question"><i class="material-icons left">question_answer</i> Questions</a></li>
-	                </div>
-                    <?php }else{?>
-                    <li><a class="dropdown-button" href="#!" data-activates="dropdown1"><i class="material-icons left">account_circle</i><?php echo $_SESSION['first_name'].' '.$_SESSION['last_name'];?><i class="material-icons right">arrow_drop_down</i></a></li>
-                    <?php }?>
-                </ul>
-                <ul id="nav-mobile" class="side-nav">
-	                    <li><img src="<?php echo BASE_URL;?>img/logo.png" style="width: 100%;" alt="<?php echo APP_TITLE;?>"></li>
-	                    <?php if(!isset($_SESSION['id'])){?>
-	                    <li><a href="<?php echo BASE_URL;?>login"><i class="material-icons left">lock_open</i>Connexion</a></li>
-	                    <li><a href="<?php echo BASE_URL;?>signup"><i class="material-icons left">open_in_new</i>Créer un compte</a></li>
-	                    <li><a href="<?php echo BASE_URL;?>question"><i class="material-icons left">question_answer</i> Questions</a></li>
-	                    <li><a href="<?php echo BASE_URL;?>search/praticien"><i class="material-icons left">search</i> Praticiens</a></li>
-                    <?php }else{?>
-                    <li><a href="<?php echo BASE_URL_ADMIN;?>"><i class="material-icons left">account_circle</i> Mon compte</a></li>
-                    <li><a href="<?php echo BASE_URL;?>search/praticien"><i class="material-icons left">search</i> Praticiens</a></li>
-                    <li><a href="<?php echo BASE_URL;?>question"><i class="material-icons left">question_answer</i> Questions</a></li>
-                    <li class="divider"></li>
-                    <li><a href="<?php echo BASE_URL;?>login/logout"><i class="material-icons left">lock_open</i> Déconnexion</a></li>
-                    <?php }?>
-                </ul>
-                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-            </div>
-        </nav>
-        <div class="container">
-        	<?php if(!isset($_SESSION['id'])){?>
-        	<div class="row">
-        		<div class="col s12 hide-on-med-and-down" style="text-align:right; padding:10px">        			
-                    <a style="padding:10px" href="<?php echo BASE_URL;?>signup">Créer un compte</a>
-                   	<a style="padding:10px" href="<?php echo BASE_URL.'login?url='.trim(BASE_URL, '/').strtok($_SERVER["REQUEST_URI"],'?');?>">Connexion</a>
-        		</div>
-        	</div>
-        	<?php }?>
+<!-- CUSTOME CSS & JS -->
+<link href="<?php echo BASE_URL;?>css/mystyle.css" rel="stylesheet" type="text/css">
+<script src="<?php echo BASE_URL;?>js/functions.js"></script>
+</head>
+<body>
+	<?php include('navbar.php');?>
+	<div class="container">
