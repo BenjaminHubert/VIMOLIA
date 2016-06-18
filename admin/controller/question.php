41677022fa0a4de3fd6a84b8b4ea107310bbedcf
Younes SADMI <younes.sadmi@gmail.com>
2016-06-18 19:41:45 +0200

    an expert can now suggest doctors when the member is not satisfy with the expert's answer

--- admin/controller/question.php
+++ admin/controller/question.php
@@ -6,8 +6,8 @@ class questionController extends baseController {
 		parent::__construct($registry);
 		// check rights
 		if(!in_array($_SESSION['role'], [
-				'Administrateur',
-				'Expert'
+			'Administrateur',
+			'Expert'
 		])){
 			$registry->template->show('403', true);
 			die();
@@ -35,7 +35,12 @@ class questionController extends baseController {
 			if($question){
 				$user = $this->registry->db->getUser($question['id_user']);
 				$questionStatus = $this->registry->db->getQuestionStatus();
+				$doctors = $this->registry->db->getAllDoctors();
 				
+				// supprimer un praticien proposé
+				if(isset($_POST['deleteProposedDoctor']) && is_numeric($_POST['deleteProposedDoctor'])){
+					$this->registry->db->deleteProposedDoctor($_POST['deleteProposedDoctor']);
+				}
 				// si on change le statut de la question
 				if(isset($_POST['status'], $_POST['changeStatus'], $_POST['idQuestion'])){
 					if(in_array($_POST['status'], $questionStatus)){
@@ -46,6 +51,8 @@ class questionController extends baseController {
 					}else
 						$this->registry->template->error = 'Statut inconnu';
 				}
+				
+				
 				// si on ajoute une réponse à la question
 				if(isset($_POST['answer'], $_POST['reply'], $_POST['idQuestion'])){
 					if($_POST['reply'] == 'addAnswer'){
@@ -61,19 +68,19 @@ class questionController extends baseController {
 							$PHPMailer->addReplyTo(EMAIL_REPLY, EMAIL_REPLY_NAME);
 							$PHPMailer->addAddress($user['email']);
 							$PHPMailer->isHTML(true);
-							$PHPMailer->Subject = '[' . APP_TITLE . ']Réponse à votre question';
+							$PHPMailer->Subject = '[' . APP_TITLE . '] Réponse à votre question';
 							$PHPMailer->Body = '
-                        	<p>Bonjour ' . (($user['pseudo'] != null) ? $user['pseudo'] : $user['first_name'] . ' ' . $user['last_name']) . '</p>
-                        	<div>
-                            	<p>Vous avez posé une question sur ' . APP_TITLE . ':</p>
-                            	<p style="font-style:italic">' . $question['question_title'] . '</p>
-                                <p style="font-style:italic">' . $question['question_text'] . '</p>
-                                <br><br>
-                                <p>Nos experts ont répondu à votre question. Vous pouvez lire là réponse en cliquant ici:</p>
-                                <a href="' . BASE_URL . 'question/afficher/' . $question['id'] . '">' . BASE_URL . 'question/afficher/' . $question['id'] . '</a>
-                                <p>A bientôt sur ' . APP_TITLE . '</p>
-                            </div>
-                        ';
+	                        	<p>Bonjour ' . (($user['pseudo'] != null) ? $user['pseudo'] : $user['first_name'] . ' ' . $user['last_name']) . '</p>
+	                        	<div>
+	                            	<p>Vous avez posé une question sur ' . APP_TITLE . ':</p>
+	                            	<p style="font-style:italic">' . $question['question_title'] . '</p>
+	                                <p style="font-style:italic">' . $question['question_text'] . '</p>
+	                                <br><br>
+	                                <p>Nos experts ont répondu à votre question. Vous pouvez lire là réponse en cliquant ici:</p>
+	                                <a href="' . BASE_URL . 'question/afficher/' . $question['id'] . '">' . BASE_URL . 'question/afficher/' . $question['id'] . '</a>
+	                                <p>A bientôt sur ' . APP_TITLE . '</p>
+	                            </div>
+	                        ';
 							if($PHPMailer->send()){
 								$this->registry->template->message = 'Un mail a été envoyé à l auteur afin de l avertir de la réponse';
 							}
@@ -83,8 +90,51 @@ class questionController extends baseController {
 					}
 				}
 				
+				// si on propose un ou plusieurs praticien(s)
+				if(isset($_POST['setProposedPraticien'], $_POST['id_question'], $_POST['id_praticien'])){
+					if(is_numeric($_POST['id_question'])){
+						$success = 0;
+						foreach($_POST['id_praticien'] as $id_praticien){
+							if($this->registry->db->addPropopsedPraticien($_SESSION['id'], $id_praticien, $_POST['id_question'])){
+								$success++;
+							}else
+								$this->registry->template->error = 'Ajout interrompu';
+						}
+						
+						if($success == count($_POST['id_praticien']) && isset($_POST['sendMail'])){
+							$PHPMailer = new MyMail();
+							$PHPMailer->setFrom(EMAIL_FROM, EMAIL_FROM_NAME);
+							$PHPMailer->addReplyTo(EMAIL_REPLY, EMAIL_REPLY_NAME);
+							$PHPMailer->addAddress($user['email']);
+							$PHPMailer->isHTML(true);
+							$PHPMailer->Subject = '[' . APP_TITLE . '] Proposition de praticiens suite à votre question';
+							$PHPMailer->Body = '
+	                        	<p>Bonjour ' . (($user['pseudo'] != null) ? $user['pseudo'] : $user['first_name'] . ' ' . $user['last_name']) . '</p>
+	                        	<div>
+	                            	<p>Vous avez posé une question sur ' . APP_TITLE . ':</p>
+	                            	<p style="font-style:italic">' . $question['question_title'] . '</p>
+	                                <p style="font-style:italic">' . $question['question_text'] . '</p>
+	                                <br><br>
+	                                <p>Nos experts ont répondu à votre question mais vous n\'en avez pas été satisfait. Nous vous proposons donc une liste de praticiens avec lesquels vous pourrez entrer en contact pour avoir plus de réponses.</p>
+	                                <a href="' . BASE_URL . 'question/afficher/' . $question['id'] . '">Cliquer ici pour voir la réponse</a>
+	                                <p>A bientôt sur ' . APP_TITLE . '</p>
+	                            </div>
+	                        ';
+							$PHPMailer->send();
+						}
+						
+						$this->registry->db->changeStatusQuestion($_POST['id_question'], "Question en attente du choix d'un praticien par le patient");
+						
+						header('Location: ' . BASE_URL_ADMIN . 'question/consult/' . $_POST['id_question']);
+						die();
+					}else
+						$this->registry->template->error = 'Les paramètres attendus sont incorrects';
+				}
+				
 				$this->registry->template->answer = $this->registry->db->getAnswerQuestion($question['id']);
 				$this->registry->template->user = $user;
+				$this->registry->template->proposed_doctors = $this->registry->db->getProposedDoctors();
+				$this->registry->template->doctors = $doctors;
 				$this->registry->template->questionStatus = $questionStatus;
 				$this->registry->template->question = $question;
 				$this->registry->template->show('consult');
--- admin/views/question/consult.php
+++ admin/views/question/consult.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row" style="border: solid 1px black; margin-top: 10px">
 	<div class="col s12">
 		<h3>Détail question</h3>
 	</div>
@@ -20,7 +20,7 @@
 			</select>
 		</div>
 		<div class="col s12 m2">
-			<button class="btn-large waves-effect waves-light" type="submit" name="changeStatus" style="width: 100%">Valider</button>
+			<button class="btn-large waves-effect waves-light blue-grey" type="submit" name="changeStatus" style="width: 100%">Valider</button>
 		</div>
 		<input type="hidden" name="idQuestion" value="<?php echo $question['id'];?>">
 	</form>
@@ -36,17 +36,19 @@
 		    	<?php echo $question['status'];?>
 		  	</div>
 		</div>
-		<p id="name"><i class="material-icons left">perm_identity</i> <?php echo ($question['pseudo']!=null)?$question['pseudo']:$question['first_name'].' '.$question['last_name'];?></p>
+		<p id="name">
+			<i class="material-icons left">perm_identity</i> <?php echo ($question['pseudo']!=null)?$question['pseudo']:$question['first_name'].' '.$question['last_name'];?></p>
 		<div class="divider"></div>
 		<p id="question"><?php echo $question['question_title'];?></p>
 		<p id="details"><?php echo $question['question_text'];?></p>
 		<div class="divider"></div>
-		<p id="datetime"><i class="material-icons right">access_time</i><?php echo date('d/m/Y à H\hm', strtotime($question['question_date']))?></p>
+		<p id="datetime">
+			<i class="material-icons right">access_time</i><?php echo date('d/m/Y à H\hm', strtotime($question['question_date']))?></p>
 	</div>
 </div>
 
-<form action="" method="POST">
-	<div class="row">
+<div class="row" style="border: solid 1px black; padding-bottom: 10px">
+	<form action="" method="POST">
 		<?php if(count($answer) == 0){?>
 		<div class="col s12">
 			<h4>Ajouter une réponse :</h4>
@@ -59,8 +61,93 @@
 		<div class="col s12 offset-m9 m3">
 			<br>
 			<br>
-			<button class="btn waves-effect waves-light" type="submit" name="reply" style="width: 100%" value="<?php echo (!isset($answer['answer_text'])?'addAnswer':$answer['id']);?>"><?php echo (isset($answer['answer_text'])?'Mettre à jour':'Ajouter');?></button>
+			<button class="btn waves-effect waves-light blue-grey" type="submit" name="reply" style="width: 100%" value="<?php echo (!isset($answer['answer_text'])?'addAnswer':$answer['id']);?>"><?php echo (isset($answer['answer_text'])?'Mettre à jour':'Ajouter');?></button>
 		</div>
+		<input type="hidden" name="idQuestion" value="<?php echo $question['id'];?>">
+	</form>
+</div>
+<div class="row" style="border: solid 1px black; padding-bottom: 10px">
+	<div class="col s12">
+		<h5>Praticiens proposés:</h5>
 	</div>
-	<input type="hidden" name="idQuestion" value="<?php echo $question['id'];?>">
-</form>
+	<div class="col s12">
+		<?php if($proposed_doctors){?>
+		<table class="bordered centered doctors">
+			<thead>
+				<tr>
+					<th>Praticien</th>
+					<th>Status</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($proposed_doctors as $proposed_doctor){?>
+				<tr>
+					<td><?php echo $proposed_doctor['first_name_praticien'].' '.$proposed_doctor['last_name_praticien'];?></td>
+					<td><?php echo implode(', ', $proposed_doctor['skills']);?></td>
+					<td>
+						<form action="" method="post">
+							<button type="submit" name="deleteProposedDoctor" value="<?php echo $proposed_doctor['id'];?>" class="btn-floating waves-effect waves-light blue-grey delete"><i class="material-icons">delete_forever</i></button>
+						</form>
+					</td>
+				</tr>
+				<?php }?>
+			</tbody>
+		</table>
+		<?php }else{?>
+		<table class="bordered centered doctors">
+			<thead>
+				<tr>
+					<th>Praticien</th>
+					<th>Status</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="no-doctor">
+					<td colspan="3" style="text-align: center">Aucun praticien proposé</td>
+				</tr>
+			</tbody>
+		</table>
+		<?php }?>
+		<br>
+		<br>
+	</div>
+	<div class="col s12 m10">
+		<select id="doctors">
+			<option disabled selected value="-1">Sélectionner un praticien</option>
+			<?php
+			foreach($doctors as $doctor){
+				$found = false;
+				foreach($proposed_doctors as $proposed_doctor){
+					if($doctor['id'] == $proposed_doctor['id_praticien']){
+						$found = true;
+					}
+				}
+				if(!$found){
+					?>
+					<option data-praticien="<?php echo $doctor['first_name'].' '.$doctor['last_name']; ?>" data-skills="<?php echo implode(', ', $doctor['skills']);?>" value="<?php echo $doctor['id'];?>">
+						<?php echo $doctor['first_name'].' '.$doctor['last_name'].' - '.implode(', ', $doctor['skills'])?>
+					</option>
+			<?php
+				}
+			}
+			?>
+		</select>
+	</div>
+	<div class="col s12 m2">
+		<button class="btn-large waves-effect waves-light blue-grey" type="submit" name="addDoctor" style="width: 100%">Ajouter</button>
+	</div>
+	<form action="" method="post">
+		<div class="col s12">
+			<p class="right">
+				<input type="checkbox" id="sendMail" name="sendMail" checked>
+				<label for="sendMail">Souhaitez-vous avertir l'auteur par mail ?</label>
+				<button class="btn waves-effect waves-light blue-grey disabled" type="submit" name="setProposedPraticien" value="submit" disabled>Enregistrer</button>
+			</p>
+		</div>
+		<div class="col s12 proposedPraticien">
+			<input type="hidden" name="id_question" value="<?php echo $question['id'];?>">
+		</div>
+	</form>
+</div>
