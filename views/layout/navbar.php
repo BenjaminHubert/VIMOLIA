36758aa2b44a7af524e438edce5564b841a82c19
Younes SADMI <younes.sadmi@gmail.com>
2016-06-18 11:39:23 +0200

    after log in/out, the user is redirect where he was

--- views/layout/navbar.php
+++ views/layout/navbar.php
@@ -3,11 +3,11 @@
 		<a id="logo-container" href="<?php echo BASE_URL;?>" class="brand-logo  center"> <img src="<?php echo BASE_URL;?>img/logo.png" alt="<?php echo APP_TITLE;?>"></a>
 		<ul id="slide-out" class="side-nav">
 			<li><img src="<?php echo BASE_URL;?>img/logo.png" style="width: 100%;" alt="<?php echo APP_TITLE;?>"></li>
-			<!-- Display "login" or "logoff" -->
+			<!-- Displaying "login" or "logoff" -->
 			<?php if(isset($_SESSION['id'])){?>
-			<li><a href="<?php echo BASE_URL;?>login/logout"><i class="material-icons left">exit_to_app</i> Déconnexion</a></li>
+			<li><a href="<?php echo BASE_URL.'login/logout?url='.getCurrentUrl();?>"><i class="material-icons left">exit_to_app</i> Déconnexion</a></li>
 			<?php }else{?>
-			<li><a href="<?php echo BASE_URL;?>login"><i class="material-icons left">lock_open</i>Connexion</a></li>
+			<li><a href="<?php echo BASE_URL.'login?url='.getCurrentUrl();?>"><i class="material-icons left">lock_open</i>Connexion</a></li>
 			<li><a href="<?php echo BASE_URL;?>signup"><i class="material-icons left">add_circle</i>Créer un compte</a></li>
 			<?php }?>
 			<li class="divider"></li>
@@ -23,12 +23,12 @@
 			<?php if(isset($_SESSION['id']) && in_array($_SESSION['role'], ['Auteur', 'Administrateur'])){?>
 			<li class="divider"></li>
 			<li><a href="<?php echo BASE_URL_ADMIN;?>article"><i class="material-icons left">description</i>Gérer les articles</a></li>
-            <li><a href="<?php echo BASE_URL_ADMIN;?>page"><i class="material-icons left">description</i>Gérer les pages</a></li>
+			<li><a href="<?php echo BASE_URL_ADMIN;?>page"><i class="material-icons left">description</i>Gérer les pages</a></li>
 			<?php }?>
 			<!-- MEMBERS & ADMINISTRATORS -->
-			<?php if(isset($_SESSION['id']) && in_array($_SESSION['role'], ['Member', 'Administrateur'])){?>
+			<?php if(isset($_SESSION['id']) && in_array($_SESSION['role'], ['Membre', 'Administrateur'])){?>
 			<li class="divider"></li>
-				<li><a href="<?php echo BASE_URL;?>question/index?mesQuestions"><i class="material-icons left">feedback</i> Mes questions</a></li>
+			<li><a href="<?php echo BASE_URL;?>question/index?mesQuestions"><i class="material-icons left">feedback</i> Mes questions</a></li>
 			<?php }?>
 			<!-- EXPERTS & ADMINISTRATORS -->
 			<?php if(isset($_SESSION['id']) && in_array($_SESSION['role'], ['Administrateur', 'Expert'])){?>
@@ -42,13 +42,13 @@
 			<li><a href="<?php echo BASE_URL_ADMIN;?>reglage"><i class="material-icons left">build</i>Réglages</a></li>
 			<?php }?>
 		</ul>
-		
+
 		<!-- RIGHT MENU -->
-		
+
 		<!-- if non loggued in -->
 		<?php if(!isset($_SESSION['id'])){?>
 		<ul class="right hide-on-med-and-down">
-			<li><a href="<?php echo BASE_URL;?>login">Connexion</a></li>
+			<li><a href="<?php echo BASE_URL.'login?url='.getCurrentUrl();?>">Connexion</a></li>
 			<li><a href="<?php echo BASE_URL;?>signup">Créer un compte</a></li>
 		</ul>
 		<?php }?>		
@@ -56,9 +56,9 @@
 		<?php if(isset($_SESSION['id'])){?>
 		<ul id="logguedIn" class="dropdown-content">
 			<li><a href="<?php echo BASE_URL_ADMIN;?>"><i class="material-icons left">account_circle</i> Mon compte</a></li>
-			<?php if(in_array($_SESSION['role'], ['Member', 'Administrateur'])){?><li><a href="<?php echo BASE_URL;?>question/index?mesQuestions"><i class="material-icons left">feedback</i> Mes questions</a></li><?php }?>
+			<?php if(in_array($_SESSION['role'], ['Membre', 'Administrateur'])){?><li><a href="<?php echo BASE_URL;?>question/index?mesQuestions"><i class="material-icons left">feedback</i> Mes questions</a></li><?php }?>
 			<li class="divider"></li>
-			<li><a href="<?php echo BASE_URL;?>login/logout"><i class="material-icons left">exit_to_app</i> Déconnexion</a></li>
+			<li><a href="<?php echo BASE_URL.'login/logout?url='.getCurrentUrl();?>"><i class="material-icons left">exit_to_app</i> Déconnexion</a></li>
 		</ul>
 		<ul class="right hide-on-med-and-down">
 			<li><a class="dropdown-button" href="#!" data-activates="logguedIn"><i class="material-icons left">person</i><?php echo ($_SESSION['pseudo'] != null)?$_SESSION['pseudo']:$_SESSION['first_name'].' '.$_SESSION['last_name']?><i class="material-icons right">arrow_drop_down</i></a></li>
