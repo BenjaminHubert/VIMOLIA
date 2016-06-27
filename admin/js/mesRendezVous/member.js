cc76326880c52434b7971bb2e82f006b71651b85
Younes SADMI <younes.sadmi@gmail.com>
2016-06-27 12:13:14 +0200

    a member can now see his rate and his comment

--- admin/js/mesRendezVous/member.js
+++ admin/js/mesRendezVous/member.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	//MODAL INIT
+	//MODAL RATE INIT
 	$('.modal-trigger.rate').leanModal({
 		dismissible: true, // Modal can be dismissed by clicking outside of the modal
 		opacity: .5, // Opacity of modal background
@@ -14,6 +14,17 @@ $(document).ready(function(){
 			}
 		}
 	);
+	//MODAL HISTORY INIT
+	$('.modal-trigger.watch').leanModal({
+		dismissible: true, // Modal can be dismissed by clicking outside of the modal
+		opacity: .5, // Opacity of modal background
+		in_duration: 300, // Transition in duration
+		out_duration: 200, // Transition out duration
+		ready: function(){}, // Callback for Modal open
+		complete: function(){  // Callback for Modal close
+			}
+		}
+	);
 	//RATE PLUGIN INIT
 	$('#rate-select').barrating({
 		theme: 'css-stars', //Specify a theme.
@@ -23,6 +34,18 @@ $(document).ready(function(){
 		deselectable: true, //If set to true, user can deselect a rating. For this feature to work the select field must contain a first option with an empty value.
 		reverse: false, //If set to true, the ratings will be reversed.
 		readonly: false, //If set to true, the ratings will be read-only.
+		fastClicks: true, //Remove 300ms click delay on touch devices.
+		hoverState: true, //Change state on hover.
+		silent: false //Supress callbacks when controlling ratings programatically.
+    });
+	$('#history-select').barrating({
+		theme: 'css-stars', //Specify a theme.
+		initialRating: null, //Specify initial rating by passing select field's option value. If null, the plugin will try to set the initial rating by finding an option with a `selected` attribute.
+		showValues: false, //If set to true, rating values will be displayed on the bars.
+		showSelectedRating: true, //If set to true, user selected rating will be displayed next to the widget.
+		deselectable: true, //If set to true, user can deselect a rating. For this feature to work the select field must contain a first option with an empty value.
+		reverse: false, //If set to true, the ratings will be reversed.
+		readonly: true, //If set to true, the ratings will be read-only.
 		fastClicks: true, //Remove 300ms click delay on touch devices.
 		hoverState: true, //Change state on hover.
 		silent: false //Supress callbacks when controlling ratings programatically.
@@ -40,6 +63,7 @@ $(document).ready(function(){
             }).done(function(data, textStatus, jqXHR){
             	if(typeof data.error == 'undefined'){
         			button.parent().hide('slow', function(){
+        				$('tr[data-id-appointment="'+id_appointment+'"] .chip-is-validated').remove();
         				$('tr[data-id-appointment="'+id_appointment+'"] .chip-is-canceled').html('Annulé');
         			});
         		}else Materialize.toast(data.error, 4000, 'red');
@@ -53,6 +77,7 @@ $(document).ready(function(){
 		var id_appointment = $(this).attr('data-id-appointment');
 		$('form.rate-appointment').attr('data-id-appointment', id_appointment);
 	});
+	
 	$('form.rate-appointment').submit(function(){
 		var id_appointment = $('form.rate-appointment').attr('data-id-appointment');
 		var button = $('a.rate[data-id-appointment="'+id_appointment+'"]');
@@ -69,23 +94,31 @@ $(document).ready(function(){
             	if(typeof data.error == 'undefined'){
             		$('form.rate-appointment .modal').closeModal();
             		button.attr('href', '#history-modal');
+            		button.attr('data-rate', rate);
+            		button.attr('data-comment', comment);
         			button.removeClass('rate');
         			button.addClass('watch');
         			button.removeClass('modal-trigger');
         			button.html('<i class="material-icons right">history</i>Voir</a>');
         			$('form.rate-appointment').removeAttr('data-id-appointment');
+        			$('a.watch').click(function(){
+        				var rate = $(this).attr('data-rate');
+        				var comment = ($(this).attr('data-comment') != '')? $(this).attr('data-comment') : 'Aucun commentaire';
+        				$('#history-modal.modal .comment').html(comment);
+        				$('#history-modal.modal #history-select').barrating('set', rate);
+        			});
         		}else Materialize.toast(data.error, 4000, 'red');
             }).fail(function(jqXHR, textStatus, errorThrown){
                 console.error(jqXHR);
             });
-			
-			
-			
-			
 		}else Materialize.toast('Veuillez au moins sélectionner une étoile', 4000, 'red');
 		return false;
 	});
+	//SEE HISTORY
 	$('a.watch').click(function(){
-		
+		var rate = $(this).attr('data-rate');
+		var comment = ($(this).attr('data-comment') != '')? $(this).attr('data-comment') : 'Aucun commentaire';
+		$('#history-modal.modal .comment').html(comment);
+		$('#history-modal.modal #history-select').barrating('set', rate);
 	});
 });
--- admin/views/mesRendezVous/member.php
+++ admin/views/mesRendezVous/member.php
@@ -16,11 +16,25 @@
 					<td><a target="_blank" href="<?php echo BASE_URL.'praticien/profile/'.$appointment['id_doctor'];?>"><?php echo $appointment['first_name_doctor'].' '.$appointment['last_name_doctor'];?></a></td>
 					<!-- IS VALIDATED -->
 					<td style="text-align:right">
-						<div class="chip chip-is-validated"><?php echo ($appointment['is_validated'] == 0)?'Non consulté':'Consulté';?></div>
+						<?php if($appointment['is_canceled'] == 1){?>
+						<!--  nothing -->
+						<?php }elseif($appointment['is_validated'] == 0){?>
+						<div class="chip chip-is-validated">Non consulté</div>
+						<?php }elseif($appointment['is_validated'] == 1){?>
+						<div class="chip chip-is-validated">Consulté</div>
+						<?php }?>
+					
+					
 					</td>
 					<!-- IS CANCEL -->
 					<td style="text-align:left">
-						<div class="chip chip-is-canceled"><?php echo ($appointment['is_canceled'] == 0)?'Non annulé':'Annulé';?></div>
+						<?php if($appointment['is_validated'] == 1){?>
+						<!--  nothing -->
+						<?php }elseif($appointment['is_canceled'] == 0){?>
+						<div class="chip chip-is-canceled">Non annulé</div>
+						<?php }elseif($appointment['is_canceled'] == 1){?>
+						<div class="chip chip-is-canceled">Annulé</div>
+						<?php }?>
 					</td>
 					<!-- ACTION BUTTON -->
 					<?php if($appointment['is_canceled'] == 0 && $appointment['is_validated'] == 0){?>
@@ -28,7 +42,7 @@
 					<?php }elseif($appointment['is_validated'] == 1 && $appointment['rating'] == null){?>
 					<td style="text-align:right"><a href="#rate-modal" class="modal-trigger waves-effect waves-light btn green lighten-2 rate" data-id-appointment="<?php echo $appointment['id'];?>"><i class="material-icons right">star_border</i>Noter</a></td>
 					<?php }elseif($appointment['is_validated'] == 1 && $appointment['rating'] != null){?>
-					<td style="text-align:right"><a class="waves-effect waves-light btn green lighten-2 watch" data-id-appointment="<?php echo $appointment['id'];?>"><i class="material-icons right">history</i>Voir</a></td>
+					<td style="text-align:right"><a href="#history-modal" class="modal-trigger waves-effect waves-light btn green lighten-2 watch" data-id-appointment="<?php echo $appointment['id'];?>" data-rate="<?php echo $appointment['rating'];?>" data-comment="<?php echo $appointment['recommendation'];?>" ><i class="material-icons right">history</i>Voir</a></td>
 					<?php }?>
 				</tr>
 				<?php }?>
@@ -76,11 +90,24 @@
 <form action="" method="post" class="history-appointment">
 	<div id="history-modal" class="modal bottom-sheet">
 		<div class="modal-content">
-			coming soon
+			<div class="row">
+				<div class="col s12">
+					<select id="history-select">
+						<option value=""></option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+					</select>
+				</div>
+				<div class="col s12">
+					<p class="comment"></p>
+				</div>
+			</div>
 		</div>
 		<div class="modal-footer">
-			<button type="submit" class="modal-action waves-effect waves-light green lighten-2 btn-flat">Valider</button>
-			<button type="button" class="modal-action modal-close waves-effect waves-red btn-flat">Annuler</button>
+			<button type="button" class="modal-action modal-close waves-effect waves-red btn-flat">Fermer</button>
 		</div>
 	</div>
 </form>
