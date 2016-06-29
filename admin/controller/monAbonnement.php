44c643987f188263dc3a68fcc4b28143e56c102c
Younes SADMI <younes.sadmi@gmail.com>
2016-06-29 10:03:42 +0200

    only doctor can access to the 'monAbonnement' section

--- admin/controller/monAbonnement.php
+++ admin/controller/monAbonnement.php
@@ -1,6 +1,17 @@
 <?php
 class monAbonnementController extends baseController {
-
+	protected $registry;
+	public function __construct($registry){
+		// default behavior
+		parent::__construct($registry);
+		// check rights
+		if(!in_array($_SESSION['role'], [
+			'Praticien'
+		])){
+			$registry->template->show('403', true);
+			die();
+		}
+	}
     public function index(){
     	
     	
