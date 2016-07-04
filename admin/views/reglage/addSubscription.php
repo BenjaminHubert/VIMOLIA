a9fe9858be4d2ce0008ac8cdaad15e3d45f9f0a8
Younes SADMI <younes.sadmi@gmail.com>
2016-07-04 21:30:25 +0200

    posted value as default value after error on form

--- admin/views/reglage/addSubscription.php
+++ admin/views/reglage/addSubscription.php
@@ -5,25 +5,25 @@
 			<div class="divider"></div>
 		</div>
 		<div class="input-field col s12 m6">
-			<input id="name" name="name" type="text" required>
+			<input id="name" name="name" type="text" value="<?php echo ($_POST['name'])??'';?>" required>
         	<label for="name">Nom de l'offre</label>
 		</div>
 		<div class="input-field col s12 m6">
-			<textarea id="description" name="description" class="materialize-textarea" length="250" required></textarea>
+			<textarea id="description" name="description" class="materialize-textarea" length="250" required><?php echo ($_POST['description'])??'';?></textarea>
         	<label for="description">Description</label>
 		</div>
 		<div class="input-field col s6 m3">
-			<input id="amount" name="amount" type="number" step="0.01" min="0.01" class="validate" style="text-align:right" required>
+			<input id="amount" name="amount" type="number" step="0.01" min="0.01" class="validate" style="text-align:right" value="<?php echo ($_POST['amount'])??'';?>" required>
         	<label for="amount" data-error="Veuillez saisir un montant valide">Montant</label>
 		</div>
 		<div class="input-field col s6 m3">
 			<select name="currencycode" id="currencycode" required>
-		    	<option value="EUR" selected>EUR (€)</option>
-		    	<option value="USD">USD ($)</option>
+		    	<option value="EUR" <?php echo (isset($_POST['currencycode']) && $_POST['currencycode'] == 'EUR')?'selected':'';?>>EUR (€)</option>
+		    	<option value="USD" <?php echo (isset($_POST['currencycode']) && $_POST['currencycode'] == 'USD')?'selected':'';?>>USD ($)</option>
 		    </select>
 		</div>
 		<div class="input-field col s12 m6">
-			<input id="duration_days" name="duration_days" type="number" step="1" min="1" class="validate" required>
+			<input id="duration_days" name="duration_days" type="number" step="1" min="1" class="validate" value="<?php echo ($_POST['duration_days'])??'';?>" required>
         	<label for="duration_days" data-error="Veuillez saisir un nombre de jours valide">Nombre de jours</label>
 		</div>
 	</div>
