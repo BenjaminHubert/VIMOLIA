39f4d9e59f5a073ac0272cbdfee18ddfe2910622
Younes SADMI <younes.sadmi@gmail.com>
2016-07-09 16:35:02 +0200

    an author can update his information

--- admin/controller/monCompte.php
+++ admin/controller/monCompte.php
@@ -15,6 +15,10 @@ class monCompteController extends baseController {
     			header('Location: '.BASE_URL_ADMIN.'monCompte/administrator');
     			die();
     			break;
+    		case 'Auteur':
+    			header('Location: '.BASE_URL_ADMIN.'monCompte/author');
+    			die();
+    			break;
     	}
     }
     
@@ -94,6 +98,32 @@ class monCompteController extends baseController {
 			}
 		}
     	$this->registry->template->show('administrator');
+    }
+    
+    public function author(){
+		if(isset($_POST['submit'])){
+			//parameters settings
+			$_POST['birthday_date'] = $_POST['birthday_submit'];
+			unset($_POST['birthday'], $_POST['birthday_submit'], $_POST['submit']);
+			$user = array_merge($_SESSION, $_POST);
+		
+			//si mise à jour de l'avatar
+			if(isset($_FILES['avatar_file']) && $_FILES['avatar_file']['size'] > 0){
+				if($_FILES['avatar_file']['error'] == UPLOAD_ERR_OK){
+					$info = pathinfo($_FILES['avatar_file']['name']);
+					$ext = $info['extension'];
+					$user['url_avatar'] = BASE_URL.'img/avatar/'.$user['id'].'.'.$ext;
+					uploadFile($_FILES, 'avatar_file', DIRECTORY_SEPARATOR.'img'.DIRECTORY_SEPARATOR.'avatar'.DIRECTORY_SEPARATOR.$user['id'].'.'.$ext);
+				}else $this->registry->template->error = 'Erreur lors de la mise à jour de la photo de profil';
+			}
+		
+			//mise à jour
+			if($this->registry->db->updateUser($user)){
+				$this->registry->template->message = 'Mise à jour effectuée avec succès';
+				$_SESSION = $user;
+			}
+		}
+    	$this->registry->template->show('author');
     }
     
 }
